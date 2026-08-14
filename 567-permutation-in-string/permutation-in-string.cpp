ff8064ebe2cc4fc281dbfdb91769748572aabacd
Sahil pramanick <sahilpramanick06@gmail.com>
2026-08-14 23:53:23 +0530

    Time: 1 ms (74.38%) | Memory: 10 MB (45.22%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,33 +1,25 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
-    }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
-        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
+        int n1 = s1.size(), n2 = s2.size();
+        if (n2 < n1) return false;
+
+        vector<int> c1(26, 0), c2(26, 0);
+
+        for (int i = 0; i < n1; i++) {
+            c1[s1[i] - 'a']++;
+            c2[s2[i] - 'a']++;
         }
+
+        if (c1 == c2) return true;
+
+        for (int i = n1; i < n2; i++) {
+            c2[s2[i] - 'a']++;
+            c2[s2[i - n1] - 'a']--;
+
+            if (c1 == c2) return true;
+        }
+
         return false;
     }
 };
